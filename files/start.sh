9b4f6e7da4df648b8a3a5cc493f317fe4afaa13b
Johannes <johannes+develop+github@braun-rheingau.de>
2021-02-14 19:54:22 +0100

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -3,4 +3,4 @@ if [ -z "$DOMAIN_SERVER" ]; then
   sed -i 's/obs.ninja/$DOMAIN_SERVER/g' /etc/nginx/nginx.conf
 fi
 
-/docker-entrypoint.sh nginx -g daemon off
+/docker-entrypoint.sh nginx -g daemon off;
